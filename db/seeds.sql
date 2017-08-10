19d4d4e02e3ce3642ccba2e09db498d3205ff748
Ryan Sebastian <Ryan Sebastian>
2017-08-09 18:37:10 -0700

    v1

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Decker Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Decker Burger');
+INSERT INTO burgers (burger_name) VALUES ('Triple Decker Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Triple Decker Burger');
+INSERT INTO burgers (burger_name) VALUES ('Triple Double Triple Decker Burger');
+INSERT INTO burgers (burger_name) VALUES ('Triple Double Triple Decker Burger with Cheese');
