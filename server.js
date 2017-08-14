ad29ec10ec078054f73876a5da2efc613114fbf6
Ryan Sebastian <Ryan Sebastian>
2017-08-14 02:15:30 -0700

    deploy fix 2 added path

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
+var path = require("path");
 
 var port = process.env.PORT || 3000;
 
