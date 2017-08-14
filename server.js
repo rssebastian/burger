1069e58031db3a83402eb2325397d377a3441381
Ryan Sebastian <Ryan Sebastian>
2017-08-14 02:09:56 -0700

    deploy fix 1

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ var app = express();
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 // Override with POST having ?_method=DELETE
 app.use(methodOverride("_method"));
